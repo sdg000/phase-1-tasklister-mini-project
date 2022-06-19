0c7ea609827d9a67a192260d8f107a06f959f5a9
Seth David Gyimah <esec26@gmail.com>
2022-06-19 00:31:38 +0000

    tasklister challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ 
+  //create form and attach event listeners
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", createNewTask);
+
+  
+  //variables for advanced modification
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+  const newTaskUl = document.getElementById("tasks");
+
+  
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+  const todoForm = document.getElementById("new-task-description");
+  const todo = document.createElement("p");
+  todo.innerText = todoForm.value;
+
+  const btn = document.createElement('button')
+  btn.textContent = "  x"
+  todo.appendChild(btn)
+  btn.addEventListener('click', del)
+
+  appendNewTask(todo);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
+
+function del(){
+  event.target.parentNode.remove()
+}
